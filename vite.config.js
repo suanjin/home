e9cdc0f26c05733a0bf15ca9f93237c7a0016957
suanjin <108645919+suanjin@users.noreply.github.com>
2023-01-17 09:44:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -62,8 +62,8 @@ export default ({
         ],
       },
       manifest: {
-        "name": "無名の主页",
-        "short_name": "無名の主页",
+        "name": "夏天の主页",
+        "short_name": "夏天の主页",
         "description": "一个默默无闻的主页",
         "display": "standalone",
         "start_url": "/",
